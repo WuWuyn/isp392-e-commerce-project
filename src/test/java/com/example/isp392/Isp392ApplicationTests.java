34d3a55a42f681f60fb37fd8a55773109c9f0596
WuWuyn <vukhanhhuyengt30@gmail.com>
2025-05-25 09:16:59 +0700

    foundation of project

---
+++ src/test/java/com/example/isp392/Isp392ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.isp392;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isp392ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
